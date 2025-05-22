5027bd70e5bb7d5f12f57625425b132df5dda33a
Inderpal1000 <inderpal.kusheldigi@gmail.com>
2025-05-23 01:43:22 +0530

    updates

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import profileImg from "/images/pf1.png";
+import profileImg from "/images/mypic.png";
 import handIcon from "/images/hand-icon.png";
 import rightArrowWhite from "/images/right-arrow-white.png";
 import downloadIcon from "/images/download-icon.png";
@@ -15,7 +15,7 @@ function Home() {
 
         <div className='flex flex-col sm:flex-row items-center gap-4 mt-4'> 
             <a href="#contact" className='px-10 py-3 border text-white rounded-full bg-gradient-to-r from-[#b820e6] to-[#da7d20] text-wrap flex items-center gap-2 dark:border-none'>contact me <img src={rightArrowWhite} alt="" className='w-4'/></a>
-            <a href="/Inderpal-resume.pdf" download className='px-10 py-3 border bg-white rounded-full border-gray-500  text-wrap flex items-center gap-2 dark:text-black'>my resume <img src={downloadIcon} alt="" className='w-4'/></a>
+            <a href="/Updated-inderpal-cv.pdf" download className='px-10 py-3 border bg-white rounded-full border-gray-500  text-wrap flex items-center gap-2 dark:text-black'>my resume <img src={downloadIcon} alt="" className='w-4'/></a>
         </div>
       </div>
     </>
